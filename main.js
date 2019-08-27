5020384b096e0a1c6b5da2c63c02504523d6d58e
Alejandro Areiza <alejandro.areiza@banistmo.com>
2019-08-27 17:54:51 -0500

    Project Initialization, basic Xpress

---
+++ main.js
@@ -0,0 +1,7 @@
+// dependencias
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+// inicializacion servidor
+app.listen(3000, () => console.log('server started'))
