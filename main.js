f497c4ad12e75f97d3bba37fcba4d5bb7485bcb4
Alejandro Areiza <alejandro.areiza@banistmo.com>
2019-08-27 22:14:54 -0500

    Conexion a mongo

--- main.js
+++ main.js
@@ -3,5 +3,11 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
+// conexion a mongo
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database'))
+
 // inicializacion servidor
 app.listen(3000, () => console.log('server started'))
